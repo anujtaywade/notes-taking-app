3d18e82104048844e6e18769647b329ba26c6d90
anuj taywade <anujtaywade456@gmail.com>
2025-07-09 21:48:17 +0530

    Update defaultNotepage.jsx

--- src/components/defaultNotepage.jsx
+++ src/components/defaultNotepage.jsx
@@ -1,19 +1,49 @@
 import React from 'react'
+import { useState } from "react";
 
 const DefaultNotepage = () => {
+  const [note, setnote] = useState(null);
+
+  let handleChange =(e)=>{
+    setnote(e.target.value)
+  }
+
+  let handleDone=(e)=>{
+    e.preventDefault()
+
+     if(!note || note.trim()==''){
+    alert("your note is empty")
+    return;
+  }
+  
+  setnote('')
+  }
+
+ 
+
   return (
     <div className='bg-purple-200 min-h-screen'>
 
         
-            <h1 className='text-4xl font-bold text-center pt-10'>Notes Taking App</h1>
+            <h1 className='text-4xl font-bold text-center pt-10 mb-5'>Notes Taking App</h1>
         
-     <div className='h-screen flex items-center justify-center  '>
+     <div className=' h-screen flex items-start justify-center  '>
       
         <input
-        className='w-4/5 h-4/5' 
+        className='w-4/5 h-2/5 ' 
+        onChange={handleChange}
         type="text"
         placeholder='start typig here'
+        value={note}
           />
+     </div>
+
+     <div className=' flex items-start justify-center '>
+      <button
+      onClick={handleDone} 
+      className=' p-3 border border-black text-black '>
+        Done
+      </button>
      </div>
     </div>
   )
