73984e1783828ded816d7d9d1a74e8af02ea4e5c
anuj taywade <anujtaywade456@gmail.com>
2025-07-13 19:21:03 +0530

    initiial commit

--- src/components/notePage.jsx
+++ src/components/notePage.jsx
@@ -8,12 +8,15 @@ const NotePage = () => {
 
   let location= useLocation();
   let note = location.state?.note || []
+  console.log(note)
 
   let redirect = () => {
     navigate("/defaultNotepage");
   };
   return (
     <div>
+
+      
       <div className="bg-purple-200 min-h-screen ">
         <div>
           <h1 className="text-4xl font-bold text-center pt-10 ">
@@ -32,11 +35,12 @@ const NotePage = () => {
         <div className="grid grid-cols-3 md:grid-cols-2">
           {Array.isArray(note)&& note.map((item,index)=>{
             <div className="text-3xl " key={index}>
-              {item}
+              the displayed note is:{item}
             </div>
           })}
         </div>
       </div>
+
     </div>
   );
 };
